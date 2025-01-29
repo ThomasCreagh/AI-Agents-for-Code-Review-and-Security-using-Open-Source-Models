3543276a01310e7c01b38a1bd90752c9c164dac6
Thomas Keatinge Creagh <tkeating@tcd.ie>
2025-01-29 14:50:37 +0000

    fixed the test? but i need to redo them

--- backend/app/api/routes/code_review_form.py
+++ backend/app/api/routes/code_review_form.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile, Depends
+from fastapi import APIRouter, File, UploadFile, Depends, Form
 
 from app.models import (
     CodeReviewFormTextBox,
@@ -10,7 +10,9 @@ router = APIRouter(prefix="/code-review-form", tags=["code-review-form"])
 
 
 @router.post("/text-box")
-def read_text_box(code_review_form_textbox: CodeReviewFormTextBox) -> dict:
+def read_text_box(
+        code_review_form_textbox: CodeReviewFormTextBox
+) -> dict:
     return TestOuput(
         returned_message="got the text correctly",
         input_value=code_review_form_textbox.model_dump()
@@ -19,12 +21,20 @@ def read_text_box(code_review_form_textbox: CodeReviewFormTextBox) -> dict:
 
 @router.post("/file-upload")
 async def read_file(
-        code_review_form: CodeReviewForm = Depends(),
-        file: UploadFile = File(...)
+        programming_language: str = Form(...),
+        error: str | None = Form(None),
+        # code_review_form: CodeReviewForm,
+        file: UploadFile = File(...),
 ) -> dict:
     return TestOuput(
         returned_message="got the file correctly",
-        input_value={"form": code_review_form.model_dump(),
-                     "filename": file.filename,
-                     "content_type": file.content_type}
+        input_value={
+            "programming_language": programming_language,
+            "error": error,
+            "filename": file.filename,
+            "content_type": file.content_type
+        }
+        # input_value={"form": code_review_form.model_dump(),
+        #              "filename": file.filename,
+        #              "content_type": file.content_type}
     ).model_dump()
