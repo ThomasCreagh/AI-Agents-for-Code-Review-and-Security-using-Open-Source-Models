03417b49399d3ffac3e1d5dbfb5ec9e9f8f26c20
Jake Casserly <jake.casserly1@gmail.com>
2025-02-06 13:40:03 +0000

    Mistake from eariler commit when trying to integrate front-end

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,11 +4,11 @@ from app.api.main import api_router
 from fastapi.routing import APIRoute
 from app.core.config import settings
 
-def custom_generate_unique_id(route: APIRoute) -> str:  # Generates unique ID for API instantiation
-    return f"{route.tags[0]}-{route.name}"
+# def custom_generate_unique_id(route: APIRoute) -> str:  # Generates unique ID for API instantiation
+#     return f"{route.tags[0]}-{route.name}"
 
 app = FastAPI(
     title=settings.PROJECT_NAME,                        # Uses config.py
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    generate_unique_id_function=custom_generate_unique_id,)
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",)
+    #generate_unique_id_function=custom_generate_unique_id,)
 app.include_router(api_router, prefix=settings.API_V1_STR)
