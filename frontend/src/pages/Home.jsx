31482401a0c01ff158b54a63470f96344c96ada7
Jake Casserly <jake.casserly1@gmail.com>
2025-02-13 22:40:25 +0000

    The get started button now navigates to the how_to_use page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/how_to_use`; 
+    navigate(path);
+  }
   return (
     <div className="container">
       <div className="content">
       <div className="text-section">
         <h1>AI Code Review & Security</h1>
         <p>Enhancing code quality and security with AI-powered insights.</p>
-        <button className="cta-button">Get Started →</button>
+        <button className="cta-button" onClick={routeChange}>Get Started →</button>
         </div>
         <div className="image-section">
           <img src="Chatbot.jpg" alt="Chatbot" />
