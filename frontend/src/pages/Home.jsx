374e6d33e4ab18750682adc57304b934dcf779a8
Thomas Keatinge Creagh <tkeating@tcd.ie>
2025-02-24 02:46:21 +0000

    fixed get started button :smile:

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,18 +3,20 @@ import "../styles/Home.css";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `/how_to_use`; 
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/how-to-use`;
     navigate(path);
-  }
+  };
   return (
     <div className="container">
       <div className="content">
-      <div className="text-section">
-        <h1>AI Code Review & Security</h1>
-        <p>Enhancing code quality and security with AI-powered insights.</p>
-        <button className="cta-button" onClick={routeChange}>Get Started →</button>
+        <div className="text-section">
+          <h1>AI Code Review & Security</h1>
+          <p>Enhancing code quality and security with AI-powered insights.</p>
+          <button className="cta-button" onClick={routeChange}>
+            Get Started →
+          </button>
         </div>
         <div className="image-section">
           <img src="Chatbot.jpg" alt="Chatbot" />
