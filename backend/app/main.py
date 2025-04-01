30123968f89db91e21ec68495e5cdbd0d7fe6db6
Thomas Keatinge Creagh <tkeating@tcd.ie>
2025-04-01 13:39:32 +0100

    adding https to cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,6 +37,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://api.keysentinel.xyz",
                    "http://keysentinel.xyz",
+                   "https://keysentinel.xyz",
                    "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
