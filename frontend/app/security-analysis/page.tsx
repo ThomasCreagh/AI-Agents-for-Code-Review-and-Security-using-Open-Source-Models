ebd863b734ccf81dd817f9abee9a7c18ad059f63
Thomas Keatinge Creagh <tkeating@tcd.ie>
2025-04-04 08:42:01 +0100

    fixing input boxes fully

--- frontend/app/security-analysis/page.tsx
+++ frontend/app/security-analysis/page.tsx
@@ -254,10 +254,10 @@ export default function SecurityCodeAnalysis() {
             <div className="file-drop-area relative flex justify-center items-center border-2 border-dashed border-[#0f62fe] rounded-lg py-10 mb-6 cursor-pointer hover:bg-[#e6f0ff] transition-all">
               <input
                 type="file"
-                className="absolute w-full h-full top-0 left-0 opacity-0 cursor-pointer"
+                className="absolute w-full h-full top-0 left-0 opacity-0 cursor-pointer z-20"
                 onChange={handleFileChange}
               />
-              <label className="flex flex-col items-center text-[#0f62fe] space-y-2 cursor-pointer relative">
+              <label className="flex flex-col items-center text-[#0f62fe] space-y-2 cursor-pointer relative z-10">
                 <span className="text-lg font-medium">
                   Drag and Drop your code file here:{" "}
                 </span>
@@ -475,10 +475,11 @@ export default function SecurityCodeAnalysis() {
                       />
                       <span className="w-4 h-4 rounded-full border-1 border-gray-400 flex items-center justify-center relative">
                         <span
-                          className={`w-2.5 h-2.5 rounded-full bg-blue-600 transition-all duration-200 ${referenceDocuments === doc
+                          className={`w-2.5 h-2.5 rounded-full bg-blue-600 transition-all duration-200 ${
+                            referenceDocuments === doc
                               ? "opacity-100"
                               : "opacity-0"
-                            }`}
+                          }`}
                         />
                       </span>
                       <span className="text-lg text-gray-700">{doc}</span>
@@ -491,10 +492,10 @@ export default function SecurityCodeAnalysis() {
                 <input
                   type="file"
                   id="document-file"
-                  className="absolute w-full h-full top-0 left-0 opacity-0 cursor-pointer"
+                  className="absolute w-full h-full top-0 left-0 opacity-0 cursor-pointer z-20"
                   onChange={handleDocumentFileChange}
                 />
-                <label className="flex flex-col items-center text-[#0f62fe] space-y-2 cursor-pointer relative">
+                <label className="flex flex-col items-center text-[#0f62fe] space-y-2 cursor-pointer relative z-10">
                   <span className="text-lg font-medium">
                     Drag and Drop your Security Documentation here:{" "}
                   </span>
